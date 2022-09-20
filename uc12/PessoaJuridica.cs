f7cafde673107cf8d3dbe8f6cc7b5b9cc1494b50
Gabriel Costa Rabelo <gabriel.costa.rabelo@gmail.com>
2022-09-20 16:47:08 -0300

    modificacoes na uc12 ER5

--- uc12/PessoaJuridica.cs
+++ uc12/PessoaJuridica.cs
@@ -10,5 +10,13 @@ namespace uc12
         public string cnpj { get; set; }
         public string razaoSocial { get; set; }
         public override void pagarimposto(float rendimento){}
+        
+        public bool ValidarCNPJ(string cnpj){
+            if( cnpj.Length >= 14 && (cnpj.Substring(cnpj.Length -4)) == "0001"){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 }
--- uc12/Program.cs
+++ uc12/Program.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 
 namespace uc12
 {
-    class Program{
+    class Program
+    {
         static void Main(string[] args)
         {
+            /*
             Endereco end = new Endereco();
             end.logradouro = "Rua x";
             end.numero = 100;
@@ -21,19 +23,38 @@ namespace uc12
             pf.dataNascimento = new DateTime (2010, 12, 25);
             //interpolação
             Console.WriteLine($"o {pf.nome} mora na {pf.endereco.logradouro} numero: {pf.endereco.numero}");
-            //concatenação
-            //Console.WriteLine("O "+ pf.nome );
             
-            //bool idadeValida = pf.ValidarDataNascimento(pf.dataNascimento);
-            //if (idadeValida == true)
-            //{
-              //  System.Console.WriteLine("Cadastro Aprovado");
-            //}
-            //else
-            //{
-             //   System.Console.WriteLine("Cadastro Reprovado");
-            //}
             
+            bool idadeValida = pf.ValidarDataNascimento(pf.dataNascimento);
+            if (idadeValida == true)
+            {
+               System.Console.WriteLine("Cadastro Aprovado");
+            }
+            else
+            {
+               System.Console.WriteLine("Cadastro Reprovado");
+            }
+            */
+            PessoaJuridica PJ = new PessoaJuridica();
+            Endereco end = new Endereco();
+            end.logradouro = "Rua x";
+            end.numero = 100;
+            end.complemento = "Qualquer coisa";
+            end.enderecoComercial = true;
+            PJ.endereco=end;
+            PJ.cnpj = "12345678990001";
+            PJ.razaoSocial = "Pessoa juridica";
+
+            bool cnpjvalido =PJ.ValidarCNPJ(PJ.cnpj);
+            Console.WriteLine(cnpjvalido);
+            if(PJ.ValidarCNPJ(PJ.
+            cnpj)){
+                Console.WriteLine("cnpj valido");
+            }else
+            {
+                Console.WriteLine("cnpj invalido");
+            }
+
         }
 
     }
